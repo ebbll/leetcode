8512c76291240daca100c56f725431d4296c4c69
이은지 <eunji@ieunjiui-MacBookAir.local>
2021-12-31 22:26:03 +0900

    minimum cost to move chips

---
+++ c/1217.c
@@ -0,0 +1,14 @@
+int minCostToMoveChips(int* position, int positionSize){
+	int even = 0, odd = 0;
+	for (int i = 0; i < positionSize; i++)\
+	{
+		if (position[i] % 2)
+			odd++;
+		else
+			even++;
+	}
+	if (odd < even)
+		return (odd);
+	else
+    	return (even);
+}
